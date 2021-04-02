d0552a6871ae2386ae0a3ded101fac523f15b185
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-02 16:28:41 +0000

    import buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
+import Button from "./Components/button";
+
 const App = () => {
-  return <div className="body">Hi</div>;
+  return (
+    <div className="body">
+      <Button name="Fast Checkout"  className='primary'/>
+      <Button name="Close" className='primary'/>
+      <Button name="Cancel Order" className='secondary cancel' />
+      <Button name="View/Cancel Order" className='tertiary'/>
+      <Button name="Keep Shopping" className='secondary'/>
+      <Button name="Add to cart"className='secondary' />
+    </div>
+  );
 };
 
 export default App;
