6744fa383faaa984f31caa0767690c4feae103de
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-02 21:46:17 +0000

    imported Checkout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Navigation from "./Navigation/Navigation";
 import Product from "./Product/Product";
+import Checkout from "./Checkout/Checkout";
 
 const App = () => {
   return (
     <div className="body">
-      <Navigation />
-      <Product />
+      {/* <Navigation />
+      <Product /> */}
+      <Checkout />
 
       {/* <Button name="Fast Checkout" className="primary" />
       <Button name="Close" className="primary" />
