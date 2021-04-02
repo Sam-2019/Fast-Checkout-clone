0ccc9896e4f9e0875bd775b78d9cd9164f4c874c
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-02 16:35:13 +0000

    added Navigation

---
+++ src/Product/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div className="company_detail">
+        <div>Logo</div>
+        <div>Name</div>
+      </div>
+
+      <div>Justify</div>
+    </div>
+  );
+};
+
+export default Navigation;
