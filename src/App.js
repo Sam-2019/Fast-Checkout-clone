b147b55836f29b9eb83073c2b42701da75efe362
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-02 17:17:43 +0000

    added Navigation, Product & commented out Buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-
-import Button from "./Components/button";
+import Navigation from "./Navigation/Navigation";
+import Product from "./Product/Product";
 
 const App = () => {
   return (
     <div className="body">
-      <Button name="Fast Checkout"  className='primary'/>
-      <Button name="Close" className='primary'/>
-      <Button name="Cancel Order" className='secondary cancel' />
-      <Button name="View/Cancel Order" className='tertiary'/>
-      <Button name="Keep Shopping" className='secondary'/>
-      <Button name="Add to cart"className='secondary' />
+      <Navigation />
+      <Product />
+
+      {/* <Button name="Fast Checkout" className="primary" />
+      <Button name="Close" className="primary" />
+      <Button name="Cancel Order" className="secondary cancel" />
+      <Button name="View/Cancel Order" className="tertiary" />
+      <Button name="Keep Shopping" className="secondary" />
+      <Button name="Add to cart" className="secondary" /> */}
     </div>
   );
 };
