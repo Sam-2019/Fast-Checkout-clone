da9596ef8a7206b27465db90446263d38d36db69
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-03 02:09:55 +0000

    created circle

---
+++ src/Components/Circle.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+let size = 30;
+
+const Circle = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="gray"
+      class="bi bi-circle"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+    </svg>
+  );
+};
+
+export default Circle;
