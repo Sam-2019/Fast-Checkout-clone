92ffb1b4396da4b3b34cf0880bd8dd4b625c0889
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-03 15:25:16 +0000

    changed product_price css to inline

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -13,29 +13,37 @@ const InsideButton = () => (
 const Product = () => {
   return (
     <>
-    <div className="product_detail">
-      <div className="image_container">
-        <img
-          src="http://www.oster.ca/on/demandware.static/-/Sites-master-catalog/default/dwca5cf622/images/highres/BLSTMB-CBG2-033-2.jpg"
-          alt="product"
-          className="product_image"
-        />
-      </div>
+      <div className="product_detail">
+        <div className="image_container">
+          <img
+            src="http://www.oster.ca/on/demandware.static/-/Sites-master-catalog/default/dwca5cf622/images/highres/BLSTMB-CBG2-033-2.jpg"
+            alt="product"
+            className="product_image"
+          />
+        </div>
 
-      <div className="name_price">
-        <div className="product_name">Oster® Pro™ Blender</div>
-        <div className="product_price">$500.99</div>
-      </div>
+        <div className="name_price">
+          <div className="product_name">Oster® Pro™ Blender</div>
+          <div
+            style={{
+              color: "gray",
+              fontSize: "20px",
+              fontWeight: "500",
+            }}
+          >
+            $500.99
+          </div>
+        </div>
 
-      <Spacer />
+        <Spacer />
 
-      <div className="product_description">
-        One of the best blenders on the market today, the Oster® Pro™ relies
-        Dual Direction Blade technology to chop, blend and grind with precision.
-        Create perfect salsas, soups and sauces with the next generation of
-        Oster® blender technology. Take your favourite smoothie to go; the
-        Oster® Pro™ includes two portable smoothie cups.
-        {/* The Oster® Pro™
+        <div className="product_description">
+          One of the best blenders on the market today, the Oster® Pro™ relies
+          Dual Direction Blade technology to chop, blend and grind with
+          precision. Create perfect salsas, soups and sauces with the next
+          generation of Oster® blender technology. Take your favourite smoothie
+          to go; the Oster® Pro™ includes two portable smoothie cups.
+          {/* The Oster® Pro™
         Performance Blender brings a powerful 1200 watts and 7 speeds to your
         kitchen. This blender includes three pre-programmed settings, making it
         instantly easy to create smoothies and other common blender creations.
@@ -43,18 +51,17 @@ const Product = () => {
         need, with the right speed and timing in just one touch. A professional
         calibre blender, the Oster® Pro™ contains an All-Metal Drive® system,
         ensuring durability and longevity. */}
+        </div>
+
+        <Spacer />
+        <Spacer />
       </div>
 
-      <Spacer />
-      <Spacer />
-
-    </div>
-
-<div className="button_container">
-<Button name={<InsideButton />} className="primary" />
-<Spacer />
-<Button name="Add to cart" className="secondary" />
-</div>
+      <div className="button_container">
+        <Button name={<InsideButton />} className="primary" />
+        <Spacer />
+        <Button name="Add to cart" className="secondary" />
+      </div>
     </>
   );
 };
