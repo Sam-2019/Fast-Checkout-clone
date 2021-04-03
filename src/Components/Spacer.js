7e2a09476e788f5e174c74e74a8c7b7a2623bb52
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-03 12:05:23 +0000

    added switch

--- src/Components/Spacer.js
+++ src/Components/Spacer.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const Spacer = () => {
-  return <div style={{ margin: "5px 0 0" }}></div>;
+let right = 10;
+
+let left = 10;
+
+const Spacer = ({ top, bottom, selected_unit }) => {
+  let unit;
+
+  switch (selected_unit) {
+    case "pixel":
+      unit = "px";
+      break;
+    case "percentage":
+      unit = "%";
+      break;
+    default:
+      unit = "px";
+  }
+
+  return (
+    <div
+      style={{
+        margin: `${top}${unit}  ${right}${unit} ${bottom}${unit}  ${left}${unit} `,
+      }}
+    ></div>
+  );
 };
 
 export default Spacer;
