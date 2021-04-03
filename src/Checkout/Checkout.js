505739bed9dd46f6ec76689374ce8763d17d14ad
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-03 14:15:20 +0000

    added select_wrapper

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -14,20 +14,29 @@ const Checkout = () => {
       <div className="company_price">
         <div>Company Name</div>
 
-     
+        <div className='select_wrapper'>
+        <img
+          src="http://www.oster.ca/on/demandware.static/-/Sites-master-catalog/default/dwca5cf622/images/highres/BLSTMB-CBG2-033-2.jpg"
+          alt="product"
+          className="option-image"
+        />
+
           <select
-            className="select"
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-          >
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
    
+          className="select"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        >
+         <option value="phone">Mobile</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+        </div>
+
+        
       </div>
 
       <Spacer
@@ -98,7 +107,7 @@ const Checkout = () => {
             borderBottom: "1px solid #d0d0d0",
           }}
         >
-          <div style={{ width: "40%" }} >
+          <div style={{ width: "40%" }}>
             <Input label="Credit Card" />
           </div>
 
@@ -129,11 +138,8 @@ const Checkout = () => {
       />
 
       <div className="terms_button">
-
-
         <Terms />
 
-    
         <div className="button_container">
           <Button name={<InsideButton />} className="primary" />
         </div>
