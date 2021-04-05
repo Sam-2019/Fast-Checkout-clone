f5b4407f4f239891340b12efecade3d547de4670
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-05 00:14:57 +0000

    added condition statement in Button, fixed loading bug

--- src/Components/button.js
+++ src/Components/button.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Spinner from "./spinner";
 import "./button.css";
 
 const Button = ({ name, action, className, loading }) => {
   return (
-    <button onClick={action} className={className} loading={loading}>
-      {name}
+    <button onClick={action} className={className} disabled={loading}>
+      {loading ? <Spinner /> : <>{name}</>}
     </button>
   );
 };
