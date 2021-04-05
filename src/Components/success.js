bedda2e60da09dd906ee70ae057681696f264bc4
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-05 15:28:44 +0000

    removed unused div, reduced svg size

--- src/Components/success.js
+++ src/Components/success.js
@@ -5,30 +5,12 @@ const Success = () => {
   return (
     <div
       style={{
-        position: "fixed",
-        left: "0",
-        right: "0",
-        top: "0",
-        bottom: "0",
-        transition: "all ease 0.7s",
-        animation: "fade-in",
-        animationDuration: "0.5s",
-        animationDelay: "0s",
-        animationFillMode: "forwards",
-        backgroundColor: "white",
-        display: "flex",
-        justifyContent: "center",
+        margin: "auto",
+        backgroundColor: "#ffffff",
+        transition: "all 0.6s ease-in-out",
       }}
     >
-      <div
-        style={{
-          margin: "auto",
-          backgroundColor: "#ffffff",
-          transition: "all 0.6s ease-in-out",
-        }}
-      >
-        <CheckCircle width={100} height={100}  color="green" />
-      </div>
+      <CheckCircle width={50} height={50} color="green" />
     </div>
   );
 };
