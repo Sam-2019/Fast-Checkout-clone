50113563a20705bdd461539394e8827005157130
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-05 11:50:09 +0000

    fixed onChange error

--- src/Components/input.js
+++ src/Components/input.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./input.scss";
 
-const Input = ({ value, onChange, label, type }) => {
+const Input = ({ value, setValue, label, type }) => {
   return (
     <>
       <div className="label_input">
         <input
           value={value}
           className="input"
-          onChange={onChange}
+          onChange={setValue}
           type={type}
           id={label}
           required
