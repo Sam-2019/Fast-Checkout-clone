fdfc626867ea215f84b44ee1a8267c0a76a3c0d8
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-05 11:50:25 +0000

    import new input

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,11 +1,29 @@
 import React from "react";
-import Input from "../Components/input";
+import Input from "../Components/input2";
 
 const Form = () => {
+  const [email, setEmail] = React.useState("");
+  const [fname, setFName] = React.useState("");
+  const [lname, setLName] = React.useState("");
+  const [number, setNumber] = React.useState("");
+  const [delivery, setDelivery] = React.useState("");
+  const [line, setLine] = React.useState("");
+  const [creditCard, setCreditCard] = React.useState("");
+  const [duration, setDuration] = React.useState("");
+  const [cvc, setCVC] = React.useState("");
+  const [zip, setZip] = React.useState("");
+
+  console.log(email);
+
   return (
     <form className="form-wrapper">
       <div>
-        <Input label="Email" />
+        <Input
+          label="Email"
+          type={email}
+          value={email}
+          setValue={(e) => setEmail(e.target.value)}
+        />
       </div>
 
       <div style={{ display: "flex" }}>
@@ -17,7 +35,11 @@ const Form = () => {
             borderRight: "1px solid #d0d0d0",
           }}
         >
-          <Input label="First Name" />
+          <Input
+            label="First Name"
+            value={fname}
+            setValue={(e) => setFName(e.target.value)}
+          />
         </div>
 
         <div
@@ -28,12 +50,21 @@ const Form = () => {
             borderTop: "1px solid #d0d0d0",
           }}
         >
-          <Input label="Last Name" />
+          <Input
+            label="Last Name"
+            value={lname}
+            setValue={(e) => setLName(e.target.value)}
+          />
         </div>
       </div>
 
       <div>
-        <Input label="Phone Number" />
+        <Input
+          label="Phone Number"
+          value={number}
+          type="number"
+          setValue={(e) => setNumber(e.target.value)}
+        />
       </div>
 
       <div style={{ display: "flex" }}>
@@ -45,7 +76,11 @@ const Form = () => {
             borderRight: "1px solid #d0d0d0",
           }}
         >
-          <Input label="Delivery Address" />
+          <Input
+            label="Delivery Address"
+            value={delivery}
+            setValue={(e) => setDelivery(e.target.value)}
+          />
         </div>
 
         <div
@@ -56,7 +91,11 @@ const Form = () => {
             borderTop: "1px solid #d0d0d0",
           }}
         >
-          <Input label="Line 2" />
+          <Input
+            label="Line 2"
+            value={line}
+            setValue={(e) => setLine(e.target.value)}
+          />
         </div>
       </div>
 
@@ -68,13 +107,30 @@ const Form = () => {
         }}
       >
         <div style={{ width: "40%" }}>
-          <Input label="Credit Card" />
+          <Input
+            label="Credit Card"
+            value={creditCard}
+            setValue={(e) => setCreditCard(e.target.value)}
+      
+          />
         </div>
 
         <div style={{ width: "60%", display: "flex", flexDirection: "row" }}>
-          <Input label="MM/YY" />
-          <Input label="CVC" />
-          <Input label="ZIP" />
+          <Input
+            label="MM/YY"
+            value={duration}
+            setValue={(e) => setDuration(e.target.value)}
+          />
+          <Input
+            label="CVC"
+            value={cvc}
+            setValue={(e) => setCVC(e.target.value)}
+          />
+          <Input
+            label="ZIP"
+            value={zip}
+            setValue={(e) => setZip(e.target.value)}
+          />
         </div>
       </div>
     </form>
