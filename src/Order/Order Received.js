aa47b6ab7b65c06d9f8bd8f839445d5b387d765d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-05 18:29:13 +0000

    removed AnotherForm

--- src/Order/Order Received.js
+++ src/Order/Order Received.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Check from "../Components/Check";
 import Spacer from "../Components/Spacer";
-import AnotherForm from "../Form/Another Form";
 import Shipping from "../Shipping/Shipping";
 import { OrderActions } from "../Order/Order";
+import Down from "../Components/chevronDown";
 
 const OrderReceived = () => {
   const [value, setValue] = React.useState("Pick your location");
+
+  const token = JSON.parse(window.localStorage.getItem("fastcheckout"));
+
   return (
     <div
       style={{
@@ -73,6 +76,7 @@ const OrderReceived = () => {
             <option value="mango">Mango</option>
           </select>
         </div>
+   
       </div>
 
       <Spacer
@@ -83,7 +87,203 @@ const OrderReceived = () => {
         selected_unit="percentage"
       />
 
-      <AnotherForm />
+<form className="form-wrapper" style={{ fontWeight: "500" }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "10px 15px",
+              border: "1px solid #d0d0d0",
+            }}
+          >
+            <label
+              style={{ color: "gray", width: "30%", padding: "0 0 0 0px" }}
+            >
+              User
+            </label>
+            <input
+              style={{
+                color: "black",
+                width: "70%",
+                outline: "none",
+                border: "1px solid transparent",
+              }}
+              placeholder="Alex Swift"
+              defaultValue={`${token.fname} ${token.lname}`}
+            />
+            <div style={{ color: "blue", width: "10%" }}>Edit</div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "10px 15px",
+              border: "1px solid #d0d0d0",
+            }}
+          >
+            <label style={{ color: "gray", width: "30%", padding: "0 0 0 0" }}>
+              Email
+            </label>
+            <input
+              style={{
+                color: "black",
+                width: "70%",
+                outline: "none",
+                border: "1px solid transparent",
+              }}
+              placeholder="alex@gmail.com"
+              defaultValue={`${token.email} `}
+            />
+            <div
+              style={{
+                width: "10%",
+              }}
+            >
+              <div
+                style={{
+                  width: "23px",
+                  height: "23px",
+                  backgroundColor: "#d5d2d2",
+                  borderRadius: "50%",
+                  padding: "4px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Down />
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "10px 15px",
+              border: "1px solid #d0d0d0",
+            }}
+          >
+            <label style={{ color: "gray", width: "30%", padding: "0 0 0 0" }}>
+              Phone
+            </label>
+            <input
+              style={{
+                color: "black",
+                width: "70%",
+                outline: "none",
+                border: "1px solid transparent",
+              }}
+              placeholder="+1 (555) 555-5555"
+              defaultValue={`${token.number} `}
+            />
+            <div
+              style={{
+                width: "10%",
+              }}
+            >
+              <div
+                style={{
+                  width: "23px",
+                  height: "23px",
+                  backgroundColor: "#d5d2d2",
+                  borderRadius: "50%",
+                  padding: "4px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Down />
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "10px 15px",
+              border: "1px solid #d0d0d0",
+            }}
+          >
+            <label style={{ color: "gray", width: "30%", padding: "0 0 0 0" }}>
+              Ship to
+            </label>
+            <input
+              style={{
+                color: "black",
+                width: "70%",
+                outline: "none",
+                border: "1px solid transparent",
+              }}
+              placeholder="120 10th Street"
+              defaultValue={`${token.delivery} `}
+            />
+            <div
+              style={{
+                width: "10%",
+              }}
+            >
+              <div
+                style={{
+                  width: "23px",
+                  height: "23px",
+                  backgroundColor: "#d5d2d2",
+                  borderRadius: "50%",
+                  padding: "4px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Down />
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "10px 15px",
+              border: "1px solid #d0d0d0",
+            }}
+          >
+            <label style={{ color: "gray", width: "30%", padding: "0 0 0 0" }}>
+              Payment
+            </label>
+            <input
+              style={{
+                color: "black",
+                width: "70%",
+                outline: "none",
+                border: "1px solid transparent",
+              }}
+              placeholder="1234"
+              defaultValue={`${token.creditCard} ${token.duration} ${token.cvc} ${token.zip} `}
+            />
+            <div
+              style={{
+                width: "10%",
+              }}
+            >
+              <div
+                style={{
+                  width: "23px",
+                  height: "23px",
+                  backgroundColor: "#d5d2d2",
+                  borderRadius: "50%",
+                  padding: "4px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Down />
+              </div>
+            </div>
+          </div>
+        </form>
+
+    
       <Spacer
         bottom={5}
         top={0}
