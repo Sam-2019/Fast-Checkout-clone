ea42ec6c97d3b1fdead1ca78b5ee2be83b1224cd
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-06 17:48:05 +0000

    removed Body style

--- src/App.js
+++ src/App.js
@@ -4,28 +4,12 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
 } from "react-router-dom";
-import styled from "styled-components";
 import Product from "./Product/Product";
 import Checkout from "./Checkout/Checkout";
 import Register from "./Register/Register";
 import OrderReceived from "./Order/Order Received";
 import Order from "./Order/Order";
-
-const Body = styled.div`
-  background-color: #ffffff34;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  transition: margin 0.5s;
-  -webkit-transition: all ease-in-out 0.5s;
-  transition: all ease-in-out 0.5s;
-`;
-
-const token = localStorage.getItem("fastcheckout");
 
 const App = () => {
   return (
